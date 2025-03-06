ebc093c6cb86b029d6892ed2dbd50e0ee7304839
lirencong <373098492@qq.com>
2025-03-06 15:04:37 +0800

    change mapbox style link

--- client/src/app/(nondashboard)/search/Map.tsx
+++ client/src/app/(nondashboard)/search/Map.tsx
@@ -22,7 +22,7 @@ const Map = () => {
 
     const map = new mapboxgl.Map({
       container: mapContainerRef.current!,
-      style: "mapbox://styles/majesticglue/cm6u301pq008b01sl7yk1cnvb",
+      style: "mapbox://styles/8863kb/cm7vb0xrr00vs01sb6e57hu6a",
       center: filters.coordinates || [-74.5, 40],
       zoom: 9,
     });
--- client/src/app/(nondashboard)/search/[id]/PropertyLocation.tsx
+++ client/src/app/(nondashboard)/search/[id]/PropertyLocation.tsx
@@ -19,7 +19,7 @@ const PropertyLocation = ({ propertyId }: PropertyDetailsProps) => {
 
     const map = new mapboxgl.Map({
       container: mapContainerRef.current!,
-      style: "mapbox://styles/majesticglue/cm6u301pq008b01sl7yk1cnvb",
+      style: "mapbox://styles/8863kb/cm7vb0xrr00vs01sb6e57hu6a",
       center: [
         property.location.coordinates.longitude,
         property.location.coordinates.latitude,
